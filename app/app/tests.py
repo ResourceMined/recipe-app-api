cc8f55c1ef645139c9cebd043e96ff16b52ca0ba
ResourceMined <129284414+ResourceMined@users.noreply.github.com>
2024-05-16 17:22:17 +0800

    linter fixes

--- app/app/tests.py
+++ app/app/tests.py
@@ -6,12 +6,13 @@ from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
     """Test calc module."""
 
     def test_add_numbers(self):
         """Test that two numbers are added together."""
-        res = calc.add(5,6)
+        res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
 
